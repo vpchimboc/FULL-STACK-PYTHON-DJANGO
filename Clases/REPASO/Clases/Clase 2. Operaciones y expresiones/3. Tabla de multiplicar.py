1fce6bf411bd4f97aeb64ec2781cfeeb3ccc2078
vpchimboc <32884862+vpchimboc@users.noreply.github.com>
2023-05-01 20:30:16 -0500

    v3

--- Clases/REPASO/Clases/Clase 2. Operaciones y expresiones/3. Tabla de multiplicar.py
+++
@@ -1,4 +0,0 @@
-num = int(input("¿Qué tabla de multiplicar quieres ver?:"))
-print(f"La tabla del {num} es:")
-for i in range(11):
-    print(f"{num} x {i} = {num * i}")
--- Clases/REPASO/Clases/Clase 7. Módulos/paquete/funcion.py
+++
@@ -1,2 +0,0 @@
-def presentarNombre(nombre):
-    return nombre
