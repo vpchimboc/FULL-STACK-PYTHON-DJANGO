a74f451ab84f34011361bebcebbe48343b9408ae
vpchimboc <32884862+vpchimboc@users.noreply.github.com>
2023-05-01 20:29:05 -0500

    v2

--- UNIDAD 3/scripts/manejo_errores/errores.py
+++
@@ -1,8 +0,0 @@
-# error de sintaxis, 
-prin(x)
-
-
-# error en tiempo de ejecución
-# cuando ejecuto e ingreso un dato inesperado, mi programa se falla
-num = int(input('INgrese un numero: '))
-print(num)
